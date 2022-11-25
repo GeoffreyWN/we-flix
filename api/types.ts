ffcfec405ff6b6d6b352e1f002c77a1b18787174
weshsnr <ndungugeoffrey11@gmail.com>
2022-11-25 15:49:53 +0300

    added an api folder

---
+++ api/types.ts
@@ -0,0 +1,22 @@
+export interface Movie  {
+    backdrop_path: string,
+    id: number,
+    original_title: string,
+    overview: number,
+    popularity: number,
+    poster_path: string,
+    title: string,
+    voter_average: number,
+    voter_count: number,
+    budget: number,
+    runtime: number,
+    revenue: number,
+    release_date: number
+}
+
+export interface Movies  {
+    page: number,
+    results: Movie[],
+    total_pages: number,
+    total_results: number,
+}
