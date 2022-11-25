da4323dee8e24f665f55ed35cb1843680c32c25e
weshsnr <ndungugeoffrey11@gmail.com>
2022-11-25 11:35:52 +0300

    set up project

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["image.tmdb.org"]
+  },
+  experimental: {
+    fontLoaders: [
+      { loader: '@next/font/google', options: { subsets: ['latin'] }}
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
+import { Raleway } from '@next/font/google'
+
+const queryClient = new QueryClient()
+
+const raleway = Raleway({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  variable: '--font-raleway'
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient} >
+      <main className={`${raleway.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
+    </QueryClientProvider>
+
+  )
 }
